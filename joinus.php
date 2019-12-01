e3f904f2041568846333211688537f0c64b0c41a
radheshantha <56368453+radheshantha@users.noreply.github.com>
2019-12-01 04:51:02 +0000

    changed background image of join us

--- joinus.php
+++ joinus.php
@@ -5,6 +5,15 @@
 </head>
 
 <body>
+  <style>
+  body{
+    background-image:linear-gradient(rgba(0,0,1,0.6),rgba(0,0,0,0.6))
+    ,url("color.jpg");
+    height: 100vh;
+    background-size: cover;
+    background-position: center;}
+</style>
+
   <nav id="MainWrapper">
   <header>
           <img class="header" src ="st_logo.png"/>
